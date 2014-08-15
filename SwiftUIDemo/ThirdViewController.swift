39ba67e3ff94c1d290bd7b99337e07c2cf2ffeb3
Richard Lee <lifuzu@gmail.com>
2014-08-15 14:48:11 -0700

    implement a simple picker view example

--- SwiftUIDemo/ThirdViewController.swift
+++ SwiftUIDemo/ThirdViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    // Properties
+    @IBOutlet var labelText: UILabel!
+    @IBOutlet var pickerView: UIPickerView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Set initial value
+        labelText.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -31,5 +35,41 @@ class ThirdViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
+}
+
+/*
+ * Reference: http://stackoverflow.com/questions/24126550/delegate-methods-in-swift-for-uipickerview
+ */
+extension ThirdViewController: UIPickerViewDataSource {
+    // Two required methods
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+        return 1
+    }
+
+    func pickerView(pickerView: UIPickerView!, numberOfRowsInComponent component: Int) -> Int
+    {
+        return 5
+    }
+}
+
+extension ThirdViewController: UIPickerViewDelegate {
+    // Several optional methods:
+
+    // func pickerView(pickerView: UIPickerView!, widthForComponent component: Int) -> CGFloat
+
+    // func pickerView(pickerView: UIPickerView!, rowHeightForComponent component: Int) -> CGFloat
+
+    func pickerView(pickerView: UIPickerView!, titleForRow row: Int, forComponent component: Int) -> String! {
+        return "\(row)"
+    }
+
+    // func pickerView(pickerView: UIPickerView!, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString!
+
+    // func pickerView(pickerView: UIPickerView!, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView!
+
+    func pickerView(pickerView: UIPickerView!, didSelectRow row: Int, inComponent component: Int) {
+        labelText.text = "\(row)"
+    }
 
 }
