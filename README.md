0e4ba9ce2249574a6eb252bca8b598b49a9c2ea7
Richard Lee <lifuzu@gmail.com>
2014-08-15 11:53:28 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Demo UI Components Using Swift/iOS 8/Xcode 6 Beta 5
+======
+
+### Functions Supported:
+Tab Controller
+Date Picker
+Picker View
+
+
+### References:
---
+++ SwiftUIDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftUIDemo
+//
+//  Created by Richard Lee on 8/15/14.
+//  Copyright (c) 2014 weimed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
