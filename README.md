6e09b7dd43ca8a9657d7d4659167d82bef6d1117
Richard Lee <lifuzu@gmail.com>
2014-08-15 16:18:12 -0700

    implement date picker

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Demo UI Components Using Swift/iOS 8/Xcode 6 Beta 5
 ======
 
 ### Functions Supported:
-Tab Controller
+Tab Bar Controller
 Date Picker
 Picker View
 
--- SwiftUIDemo/SecondViewController.swift
+++ SwiftUIDemo/SecondViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-                            
+
+    // Properties
+    @IBOutlet var labelText: UILabel!
+    @IBOutlet var datePicker: UIDatePicker!
+    var selectedDay: NSDate?
+    var dateFormatter: NSDateFormatter?
+
+    @IBAction func datePickerChanged(sender: AnyObject) {
+        var selectedDate = (sender as UIDatePicker).date
+        self.labelText.text = self.dateFormatter!.stringFromDate(selectedDate)
+        self.selectedDay = selectedDate
+    }
+
+    func setupLabelText() {
+        dateFormatter = NSDateFormatter()
+        dateFormatter!.dateStyle = .MediumStyle
+        dateFormatter!.timeStyle = .MediumStyle
+
+        var defaultDate = NSDate.date()
+
+        self.labelText.text = dateFormatter!.stringFromDate(defaultDate)
+        //self.labelText.textColor = Color
+
+        self.selectedDay = defaultDate
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Set an initial value
+        setupLabelText()
+        datePicker.date = self.selectedDay
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
